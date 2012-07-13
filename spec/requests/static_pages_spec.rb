13a082ab8ede551def72384984ba9175ba4a341e
Frandroid <frandroid@gmail.com>
2012-07-13 16:46:56 -0400

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'     
+
+describe "Static pages" do
+
+   describe "Home page" do
+ 
+      it "should have the content 'Great Worm'" do
+         visit '/static_pages/home'
+         page.should have_selector('h1', :text => 'Great Worm')
+      end
+    
+      it "should have the title 'Home'" do
+         visit '/static_pages/home'
+         page.should have_selector('title',
+                          :text => "Home | Great Worm Express Distro")
+      end
+    
+      
+   end
+ 
+   describe "Help page" do
+ 
+      it "should have the content 'Help'" do
+         visit '/static_pages/help'
+         page.should have_content('Help')
+      end
+   end
+ 
+   describe "About page" do
+ 
+      it "should have the content 'About Great Worm'" do
+         visit '/static_pages/about'
+         page.should have_content('About Great Worm')
+      end
+
+      it "should have the title 'About'" do
+        visit '/static_pages/about'
+        page.should have_selector('title',
+                          :text => "About | Great Worm Express Distro")
+      end
+
+   end
+   
+   describe "Order page" do
+ 
+      it "should have the content 'How to Order'" do
+         visit '/static_pages/order'
+         page.should have_content('How to Order')
+      end
+   end
+   
+   
+end
